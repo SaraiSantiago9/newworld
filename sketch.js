903c8ba0b520dd04611bc573a3478cec427f3daf
SaraiSantiago9 <133685257+SaraiSantiago9@users.noreply.github.com>
2023-05-15 12:16:34 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+
+//The setup function only happens once
+function setup () {
+	createCanvas (500,500); //create a 500px X 500px canvas
+}
+
+//The draw function happens over and over again
+function draw() {
+	background(208,207,208); //an RGB color for the canvas' background
+	//cricle
+	stroke()
+}
